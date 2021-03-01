7bb680a743cf5d572d66ba9fc84854996b7e641b
Co-7 <48591820+Co-7@users.noreply.github.com>
2021-03-01 12:15:49 +0100

    Create http-client.js

---
+++ learnyounode/http-client.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+http.get(process.argv[2], res => {
+
+    res.setEncoding("utf8");
+    res.on("data", data => console.log(data));
+    res.on("error", err => console.error(err));
+
+});
